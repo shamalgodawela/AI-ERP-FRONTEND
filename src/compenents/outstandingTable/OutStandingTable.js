dbdf35f987c6e27fe1bad28655aad027a986a8a5
shamal melantha <shamalmelantha3@gmail.com>
2025-09-24 10:28:21 +0530

    Your commit message

--- src/compenents/outstandingTable/OutStandingTable.js
+++ src/compenents/outstandingTable/OutStandingTable.js
@@ -202,6 +202,8 @@ const OutStandingTable = () => {
             <option value="SOUTH">SOUTH-1</option>
             <option value="Other">Other</option>
             <option value="UpCountry">UpCountry</option>
+            <option value="Miss.Mubashshahira">Miss.Mubashshahira</option>
+            <option value="Mr.Buddhika">Mr.Buddhika</option>
           </select>
 
           <input
