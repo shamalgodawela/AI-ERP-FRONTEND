98f3172cd117d0513bd2cffa429a1c9a2ccdca53
shamal melantha <shamalmelantha3@gmail.com>
2026-08-03 09:49:39 +0530

    Comment out AdminRoute and related imports in App.js and AdminRoute.js for potential future use; adjust routing structure for clarity.

--- src/App.js
+++ src/App.js
@@ -107,7 +107,7 @@ import StockSnapshot from "./pages/StockSnapsot/StockSnap";
 import FinancialReportEditor from "./pages/Accountdepartment/FinancialReport/Financereport";
 import InventoryByDescription from "./pages/dashboard/AdminDASH/Dashboard";
 import Getallreturnadmin from "./pages/returnNotes/gettall/Getallreturnadmin";
-import AdminRoute from "./routes/AdminRoute";
+// import AdminRoute from "./routes/AdminRoute";
 
 
 
@@ -145,8 +145,8 @@ function App() {
 
 {/* ---------------------------------Admin Pages--------------------------------- */}
 
-<AdminRoute/>
-    {/* <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+
+    <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
       
       <Route path="/adminorder/:id" element={<Oneorder/>} />
       <Route path="/Exe-product-wise-sales" element={<SalesByExe/>} />
@@ -179,7 +179,7 @@ function App() {
     <Route path="/getall-cheques" element={<Getallcheque/>} />
     <Route path="/Admin-invoice" element={<AdminInvoice/>} />
     <Route path="/Admin-invoice-return" element={<Getallreturnadmin/>} />
-    </Route> */}
+    </Route>
 
 {/* ----------------------------------------------------------------------------- */}
 
